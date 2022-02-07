7f884458efece70fb81985d06b03f88ba887616b
Eduardo Melo <92065918+eduardoddmg@users.noreply.github.com>
2022-02-06 22:27:55 -0300

    Add files via upload

---
+++ src/main.js
@@ -0,0 +1,25 @@
+import './assets/css/style.css';
+import { geradorCpf } from './modules/validaCPF'
+
+const resultado = document.querySelector('.resultado');
+const btn = document.querySelector('.btn');
+let counter = 5;
+
+btn.addEventListener('click', () => {
+    const timer = setInterval(() => {
+        if (counter > 0) resultado.innerHTML = `Seu CPF será gerado em ${counter}...`;
+        else if (counter == 0) resultado.innerHTML = `...`;
+        else 
+        {
+            resultado.innerHTML = geradorCpf();
+            clearInterval(timer)
+        }
+        counter--;
+    }, 1000);
+    counter = 5;
+});
+
+
+
+
+
---
+++ src/modules/validaCPF.js
@@ -0,0 +1,76 @@
+let cpfAleatorio = '';
+
+function rand(max, min)
+{
+    return Math.floor(Math.random()*(max-min)+min);
+}
+
+let cpf = rand(100000000, 999999999);
+let numeros = []
+for (let i = 1; i < 10; i++)
+{
+    numeros.unshift(parseInt(cpf%(10**1)));
+    cpf = cpf/(10**1)
+}
+
+
+class VerificadorCpf1 {
+    constructor(cpf, valor_multiplicado, valor_soma, aux, counter)
+    {
+        this.cpf = cpf;
+        this.valor_multiplicado = valor_multiplicado;
+        this.valor_soma = valor_soma;
+        this.aux = aux;
+        this.counter = counter;
+        for (; this.counter < 1; this.counter++)
+        {
+            for (let i of cpf)
+             {
+                 this.valor_multiplicado =  i*aux;
+                 this.valor_soma += this.valor_multiplicado;
+                 aux--;
+             }    
+             this.valor = 11 - (this.valor_soma % 11);
+             if (this.valor > 9) this.valor = 0;
+             numeros.push(this.valor);
+             counter++;
+        }             
+    }    
+}    
+
+class VerificadorCpf2 {
+    constructor(cpf, valor_multiplicado, valor_soma, aux, counter)
+    {
+        this.cpf = cpf;
+        this.valor_multiplicado = valor_multiplicado;
+        this.valor_soma = valor_soma;
+        this.aux = aux;
+        this.counter = counter;
+        for (; this.counter < 1; this.counter++)
+        {
+            for (let i of cpf)
+             {
+                 this.valor_multiplicado =  i*aux;
+                 this.valor_soma += this.valor_multiplicado;
+                 aux--;
+             }    
+             this.valor = 11 - (this.valor_soma % 11);
+             if (this.valor > 9) this.valor = 0;
+             numeros.push(this.valor);
+             counter++;
+        }             
+    }    
+}
+    function geradorCpf()
+    {
+        new VerificadorCpf1(numeros, 0,0,numeros.length+1,0);
+        new VerificadorCpf2(numeros,0,0,numeros.length+1,0);
+        numeros = String(numeros);
+        numeros = numeros.replaceAll(',','');
+        return numeros;
+    }
+
+
+
+
+export { geradorCpf };
